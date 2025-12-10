fd580432c93bdcc6468020bd66006bc67eab12f6
Nabil Afia <nabil.afia.afia@gmail.com>
2025-12-11 00:29:53 +0100

    feat: Implement the player rotation

--- main.py
+++ main.py
@@ -2,17 +2,22 @@ import pygame
 from constants import SCREEN_WIDTH 
 from constants import SCREEN_HEIGHT 
 from logger import log_state
+from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock_object = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black") 
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
         dt = clock_object.tick(60)/1000
 
