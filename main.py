d7453859def8884335772a1dad34c1c5a388e0d9
Nabil Afia <nabil.afia.afia@gmail.com>
2025-12-11 01:40:18 +0100

    feat: Implemnet the collision meacanisem

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
 import pygame
 from constants import SCREEN_WIDTH 
 from constants import SCREEN_HEIGHT 
+import sys
 from logger import log_state
+from logger import log_event 
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock_object = pygame.time.Clock()
     dt = 0
+    updatable= pygame.sprite.Group()
+    drawable= pygame.sprite.Group()
+    asteroids =  pygame.sprite.Group()
+    Asteroid.containers = (asteroids,updatable,drawable) 
+    Player.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable)
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
-
+    asteroid_field = AsteroidField()
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black") 
-        player.draw(screen)
-        player.update(dt)
+        for draw in drawable:
+            draw.draw(screen)
+
+        for ast in asteroids:
+            if ast.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
+        updatable.update(dt)
         pygame.display.flip()
         dt = clock_object.tick(60)/1000
 
